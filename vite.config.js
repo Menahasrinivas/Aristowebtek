e8d084f2c90a028b8e582e590febb6041ee701e0
S.Menaha <menahasrinivas@gmail.com>
2025-12-01 12:04:40 +0530

    updated base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: "/aristo/",
+   base: "/",
    server:{
     host:"0.0.0.0"
    } // 👈 important
